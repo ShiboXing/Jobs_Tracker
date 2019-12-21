89f7a63d85853949fbf7907b3065674cbba80862
--- <shx26@pitt.edu>
2019-12-21 16:53:51 +0800

    added components.js

---
+++ pitt-social-app/src/Components.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.css'
+
+class Sidebar extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    test test test
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default Sidebar
--- pitt-social-app/src/index.js
+++ pitt-social-app/src/index.js
@@ -1,32 +1,34 @@
 ﻿import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import Sidebar from './Components'
+
 //import * as serviceWorker from './serviceWorker'
 //ServiceWorker deprecated ???
 
 class MainBody extends React.Component {
     render() {
         return (
-            <html>
-                <link href="https://fonts.googleapis.com/css?family=Alata|Wallpoet&display=swap" rel="stylesheet"/> 
-                <head>
-                    <title> P I T T   S O C I A L </title>
-                </head>
-                <body>
-
-                    <h1 class='top_heading'>Pitt Social<sup class='top_heading'>2</sup> </h1>
-                    <p> Welcome to Pitt Social<sup>2</sup> , a platform for pitt affiliated people to social </p>
-                    <hr />
-                    <p> feel free to social, if you are a &nbsp;
-                        <abbr title='pitt affiliated person'>PAP</abbr>
-                    </p>
-                    <h2> How to social </h2>
-                    <p> Add friends, add groups -> Send messages <br/>
-                        You come, you see -> you conquer
-                    </p>
-                 
-                </body>
-            </html>
+            <div>
+                <link href='https://fonts.googleapis.com/css?family=Alata|Wallpoet&display=swap' rel='stylesheet'/> 
+                <div style={{border: '5px solid blue'}}>
+                    <div className='LeftPartSecond'>
+                        <h1 className='top_heading'>Pitt Social<sup className='top_heading'>2</sup> </h1>
+                        <p> Welcome to Pitt Social<sup>2</sup> , a platform for pitt affiliated people to social </p>
+                        <hr />
+                        <p> feel free to social, if you are a &nbsp;
+                            <abbr title='pitt affiliated person'>PAP</abbr>
+                        </p>
+                        <h2> How to social </h2>
+                        <p> Add friends, add groups -> Send messages <br/>
+                            You come, you see -> you conquer
+                        </p>
+                    </div>
+                    <div className='RightPartFirst'>
+                        <Sidebar></Sidebar>
+                    </div>
+                </div>
+            </div>
         )
     }    
 }
